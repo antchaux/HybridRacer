93baa85a92c7b245fcbad3047256aea48d97bf85
Shempyop <antoine.chaux@jakarto.com>
2018-11-21 19:55:52 -0500

    asset import + car on road

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	private Rigidbody2D rb;
+	private float moveInput;
+	public float roadOffset;
+
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	void Update(){
+		moveInput = Input.GetAxisRaw("Horizontal");
+		if(Input.GetKeyDown(KeyCode.LeftArrow) && rb.position.x >= 0){
+			Debug.Log(moveInput);
+			rb.position = new Vector2(rb.position.x + moveInput - roadOffset, rb.position.y);
+		}
+		else if(Input.GetKeyDown(KeyCode.RightArrow) && rb.position.x <= 0){
+			Debug.Log(moveInput);
+			rb.position = new Vector2(rb.position.x + moveInput + roadOffset, rb.position.y);
+		}
+	}
+}
